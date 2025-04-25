0af5cb37394b5e0be768670b904b2bb8cd56a24a
ThinBucket82549 <73562905+ThinBucket82549@users.noreply.github.com>
2025-04-25 17:19:32 +1000

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="OneDrive Vercel Index" />
+          <meta name="description" content="AussieBucket's Drive full of random things. very epic. :D" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -24,3 +24,5 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
+ 
